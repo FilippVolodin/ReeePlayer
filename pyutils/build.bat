5ae6229ee752d291cb3a6017a5a94c0f943ec297
Filipp Volodin <filipp.v@gmail.com>
2023-03-26 21:00:22 +0300

    Update installer

--- pyutils/build.bat
+++ pyutils/build.bat
@@ -1,6 +1,6 @@
 python -m venv env
 call env\Scripts\activate
-REM python -m pip install numpy onnxruntime soundfile pyinstaller
+REM python -m pip install numpy onnxruntime soundfile pyinstaller genanki
 pip install -r requirements.txt
 pyinstaller -y pyutils.spec
 call env\Scripts\deactivate
